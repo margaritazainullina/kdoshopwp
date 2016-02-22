b8937e6bf7732dbd53126acc944bafdb017fbe5c
spasskaya_m <margarita.spaakaya@outlok.com>
2016-02-22 18:19:30 +0100

    credentials

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp');
+define('DB_NAME', 'php');
 
 /** MySQL database username */
-define('DB_USER', 'adminiJwiql1');
+define('DB_USER', 'adminssY2rzg');
 
 /** MySQL database password */
-define('DB_PASSWORD', '6Ix1G45QlgZl');
+define('DB_PASSWORD', 'azUd99uPaCQQ');
 
 /** MySQL hostname */
-define('DB_HOST', 'wp-kdoshop.rhcloud.com');
+define('DB_HOST', 'php-kdoshop.rhcloud.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
